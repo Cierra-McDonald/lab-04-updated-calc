a39f5e34eb048f24b637c68d455335aa50c4d85d
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-08 15:36:07 -0700

    calculator including add,substract, multiply, and division features

---
+++ mathUtils.js
@@ -0,0 +1,12 @@
+export function getSum(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+export function beDifferent(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+export function getDivision(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+export function getProduct(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
